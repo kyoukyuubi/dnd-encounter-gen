2cb3ebb32839ad981621b0bf928b28aeb014ae16
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-03-12 18:55:28 +0100

    Made the function that sorts after challenge rating. Added the random module and made some formatting for the found creature

--- src/data_handling.py
+++ src/data_handling.py
@@ -28,7 +28,7 @@ def prepare_creatures(args):
     creatures = []
     creatures_sorted = []
 
-    # set the arg values
+    # set the arg values for env and plane
     environment = args.environment
     plane = args.plane
 
@@ -39,6 +39,10 @@ def prepare_creatures(args):
         print(e)
         return
     
+    #if environment and plane is none, return the loaded creatures since we don't need to do any checks on them here
+    if environment is None and plane is None:
+        return creatures
+
     # loop through the loaded creatures
     for creature in creatures:
         # check the creatures if environment has an element that matches and add the creature to the list
@@ -56,3 +60,100 @@ def prepare_creatures(args):
 
     # return the list
     return creatures_sorted
+
+def calc_creatures(creatures, args):
+    # set the dict that stores the challenge rating for set level and party size
+    solo_creature = {
+        4: {
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 7,
+            6: 8,
+            7: 9,
+            8: 10,
+            9: 11,
+            10: 12,
+            11: 13,
+            12: 15,
+            13: 16,
+            14: 17,
+            15: 18,
+            16: 19,
+            17: 20,
+            18: 20,
+            19: 21,
+            20: 23,
+        },
+        5: {
+            1: 2,
+            2: 3,
+            3: 4,
+            4: 5,
+            5: 8,
+            6: 9,
+            7: 10,
+            8: 11,
+            9: 12,
+            10: 13,
+            11: 14,
+            12: 16,
+            13: 17,
+            14: 18,
+            15: 19,
+            16: 20,
+            17: 21,
+            18: 21,
+            19: 22,
+            20: 23,
+        },
+        6:{
+            1: 2,
+            2: 4,
+            3: 5,
+            4: 6,
+            5: 9,
+            6: 10,
+            7: 11,
+            8: 12,
+            9: 13,
+            10: 14,
+            11: 15,
+            12: 17,
+            13: 18,
+            14: 19,
+            15: 20,
+            16: 21,
+            17: 22,
+            18: 22,
+            19: 23,
+            20: 24,
+        },
+    }
+
+    # prepare the result list
+    result_creatures = []
+
+    # set the arg values for level and size
+    level = args.level
+    size = args.size
+
+    # check if level is within the allowed range of 1-20
+    if level < 1 or level > 20:
+        raise Exception(f"The allowed level is between 1 and 20. A level of {level} is not allowed")
+    
+    # check if the group size is between the allowed range on 4-6
+    if size < 4 or size > 6:
+        raise Exception(f"The allowed size is between 4 and 6. A size of {size} is not allowed")
+    
+    # get the correct challenge level
+    challenge_rating = str(solo_creature[size][level])
+    
+    # loop through the creatures list and append the ones with a matching challenge rating to result_creatures
+    for creature in creatures:
+        if creature["ChallengeRating"] == challenge_rating:
+            result_creatures.append(creature)
+
+    # return the list
+    return result_creatures
--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
-from data_handling import prepare_creatures
+import random
+from data_handling import prepare_creatures, calc_creatures
 from input_handling import parse_arguments
 
 def main():
     args = parse_arguments()
 
-    creatues = prepare_creatures(args)
-    for create in creatues:
-        print(create['Name'])
+    creatures = prepare_creatures(args)
+    creatures_sorted = calc_creatures(creatures, args)
+    random_creature = random.choice(creatures_sorted)
+    print("Creature Found!")
+    print("----------------------------------------------")
+    print(f"Name: {random_creature['Name']}")
+    print(f"Type: {random_creature['Type']}")
+    print(f"Challenge Rating: {random_creature['ChallengeRating']}")
+    print(f"Environment: {random_creature['Environment']}")
+    # check if plane is not empty, if it isn't print what plane the creature is on
+    if random_creature["Plane"] is not None:
+        print(f"Plane: {random_creature["Plane"]}")
+    # a space for formatting
+    print(" ")
+    print(f"The creature can be found in: {random_creature["Book"]} on page: {random_creature["Page"]}")
+    print("----------------------------------------------")
 
 if __name__ == "__main__":
     main()
