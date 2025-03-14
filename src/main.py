b58e38745ad50cee6a450b6bf5f47453801b0795
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-03-14 15:59:29 +0100

    Reverted main.py to display the results correctly. It had debug pritns

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from input_handling import parse_arguments
 def main():
     args = parse_arguments()
     encounter = generate_encounter(args, seed=45)
-    print(encounter)
-    # display_encounter(encounter)
+    display_encounter(encounter)
 
 if __name__ == "__main__":
     main()
