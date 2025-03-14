580ec2bd9d3bfba5d25594fb33d8305614898caf
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-03-14 15:58:39 +0100

    Added some test cases for the current data_base

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ from input_handling import parse_arguments
 
 def main():
     args = parse_arguments()
-    encounter = generate_encounter(args)
-    display_encounter(encounter)
+    encounter = generate_encounter(args, seed=45)
+    print(encounter)
+    # display_encounter(encounter)
 
 if __name__ == "__main__":
     main()
